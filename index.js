5b6db68914d3423ead4c1f7f50cd8774750b63ed
Manuel velasco <manuelvelasco2020@gmail.com>
2023-05-28 19:10:27 -0700

    Adding Firebase to project

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  push,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const appSettings = {
+  databaseURL: 'https://mobile-app-database-6a9a3-default-rtdb.firebaseio.com/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDBase = ref(database, 'shoppingList');
+
 const inputEl = document.getElementById('input-el');
 const buttonEl = document.getElementById('button-el');
 
 buttonEl.addEventListener('click', function () {
   let inputFieldValue = inputEl.value;
+
+  push(shoppingListInDBase, inputFieldValue);
 
   console.log(inputFieldValue);
 });
